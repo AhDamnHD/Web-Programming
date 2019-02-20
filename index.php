75a7f552ef377ee359eb392bef32215a7fce9b90
Adam Halley <Ag3ntMayn3@gmail.com>
2019-02-20 21:43:10 +0000

    TEST

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+    <div class="title">
+    <?php
+        echo "<h1>COMP606/1901</h1>";
+        echo "<hr/>";
+        echo "<p>Welcome to COMP606/1901</p>";
+    ?>
+    </div>
+<a href="profile.html">Visit my profile.</a>
+</div>
+
+</body>
+</html>
